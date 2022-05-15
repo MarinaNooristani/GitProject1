cb88a94a716668d255e44763f2ee59548cab278d
MarinaNooristani <marina.nooristani321@gmail.com>
2022-05-15 20:56:53 +0300

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("I am Great!!!!!");
+        System.out.println("I am Great222!!!!!");
+        System.out.println("I am Great333333!!!!!");
     }
 }
